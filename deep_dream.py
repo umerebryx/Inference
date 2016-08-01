ba17a9bf5777f1c95fbd7787257a0df6b6744948
Umer Khan <umer.khan@ebryx.com>
2016-08-01 20:07:15 +0500

    initial commit

---
+++ deep_dream.py
@@ -0,0 +1,30 @@
+
+from __future__ import print_function
+import os
+from io import BytesIO
+import numpy as np
+from functools import partial
+import PIL.Image
+#from IPython.display import clear_output, Image, display, HTML
+#from __future__ import print_function
+
+import tensorflow as tf
+
+
+model_fn = 'export'
+
+graph = tf.Graph()
+sess = tf.InteractiveSession(graph=graph)
+with tf.gfile.FastGFile(model_fn, 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+t_input = tf.placeholder(np.float32, name='input') # define the input tensor
+imagenet_mean = 117.0
+t_preprocessed = tf.expand_dims(t_input-imagenet_mean, 0)
+tf.import_graph_def(graph_def, {'input':t_preprocessed})
+layers = [op.name for op in graph.get_operations() if op.type=='Conv2D' and 'import/' in op.name]
+feature_nums = [int(graph.get_tensor_by_name(name+':0').get_shape()[-1]) for name in layers]
+
+print('Number of layers', len(layers))
+print('Total number of feature channels:', sum(feature_nums))
+
