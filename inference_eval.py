a75acfbd9d1880919c00b4aee5460cb69259311d
Umer Khan <umer.khan@ebryx.com>
2016-08-07 14:11:04 +0500

    loading model added

---
+++ inference_eval.py
@@ -0,0 +1,190 @@
+"""A library to evaluate Inception on a single GPU.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from datetime import datetime
+import math
+import os.path
+import time
+
+
+import numpy as np
+import tensorflow as tf
+import image_processing
+from inception_model as inception
+
+import os
+from io import BytesIO
+from functools import partial
+import PIL.Image
+from IPython.display import clear_output, Image, display, HTML
+
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_string('eval_dir', '/tmp/imagenet_eval',
+                           """Directory where to write event logs.""")
+tf.app.flags.DEFINE_string('checkpoint_dir', '/tmp/imagenet_train',
+                           """Directory where to read model checkpoints.""")
+
+# Flags governing the frequency of the eval.
+tf.app.flags.DEFINE_integer('eval_interval_secs', 60 * 5,
+                            """How often to run the eval.""")
+tf.app.flags.DEFINE_boolean('run_once', False,
+                            """Whether to run eval only once.""")
+
+# Flags governing the data used for the eval.
+tf.app.flags.DEFINE_integer('num_examples', 50000,
+                            """Number of examples to run. Note that the eval """
+                            """ImageNet dataset contains 50000 examples.""")
+tf.app.flags.DEFINE_string('subset', 'validation',
+                           """Either 'validation' or 'train'.""")
+
+Sess = None
+img_noise = np.random.uniform(size=(224,224,3)) + 100.0
+
+def tffunc(*argtypes):
+    '''Helper that transforms TF-graph generating function into a regular one.
+    See "resize" function below.
+    '''
+    placeholders = list(map(tf.placeholder, argtypes))
+    def wrap(f):
+        out = f(*placeholders)
+        def wrapper(*args, **kw):
+            # This line is causing the issue, the session I am passing is different from the tesor flow default:
+            # Below is the exception I get:
+            #ValueError: Cannot use the default session to evaluate tensor: the tensor's graph is different from the session's graph.
+            # Pass an explicit session to `eval(session=sess)`.
+            return out.eval(dict(zip(placeholders, args)), session=Sess)
+        return wrapper
+    return wrap
+
+def T(layer):
+    '''Helper for getting layer output tensor'''
+    return graph.get_tensor_by_name("%s:0"%layer)
+
+def showarray(a, fmt='jpeg'):
+    a = np.uint8(np.clip(a, 0, 1)*255)
+    f = BytesIO()
+    PIL.Image.fromarray(a).save(f, fmt)
+
+
+def resize(img, size):
+    img = tf.expand_dims(img, 0)
+    return tf.image.resize_bilinear(img, size)[0,:,:,:]
+resize = tffunc(np.float32, np.int32)(resize)
+
+def render_deepdream(t_obj, img0=img_noise,
+                     iter_n=10, step=1.5, octave_n=4, octave_scale=1.4):
+    t_score = tf.reduce_mean(t_obj) # defining the optimization objective
+    t_grad = tf.gradients(t_score, t_input)[0] # behold the power of automatic differentiation!
+
+    # split the image into a number of octaves
+    img = img0
+    octaves = []
+    for i in range(octave_n-1):
+        hw = img.shape[:2]
+        lo = resize(img, np.int32(np.float32(hw)/octave_scale))
+        hi = img-resize(lo, hw)
+        img = lo
+        octaves.append(hi)
+    
+    # generate details octave by octave
+    for octave in range(octave_n):
+        if octave>0:
+            hi = octaves[-octave]
+            img = resize(img, hi.shape[:2])+hi
+        for i in range(iter_n):
+            g = calc_grad_tiled(img, t_grad)
+            img += g*(step / (np.abs(g).mean()+1e-7))
+            print('.',end = ' ')
+        clear_output()
+        showarray(img/255.0)
+
+def _eval_once(saver, summary_writer, top_1_op, top_5_op, summary_op):
+  """Runs Eval once.
+  Args:
+    saver: Saver.
+    summary_writer: Summary writer.
+    top_1_op: Top 1 op.
+    top_5_op: Top 5 op.
+    summary_op: Summary op.
+  """
+  with tf.Session() as sess:
+    ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
+    if ckpt and ckpt.model_checkpoint_path:
+      if os.path.isabs(ckpt.model_checkpoint_path):
+        # Restores from checkpoint with absolute path.
+        saver.restore(sess, ckpt.model_checkpoint_path)
+      else:
+        # Restores from checkpoint with relative path.
+        saver.restore(sess, os.path.join(FLAGS.checkpoint_dir,
+                                         ckpt.model_checkpoint_path))
+
+      # Assuming model_checkpoint_path looks something like:
+      #   /my-favorite-path/imagenet_train/model.ckpt-0,
+      # extract global_step from it.
+      global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+      print('Succesfully loaded model from %s at step=%s.' %
+            (ckpt.model_checkpoint_path, global_step))
+    else:
+      print('No checkpoint file found')
+      return
+    #Load graph from checkpoint
+    t_input = tf.placeholder(np.float32, name='input') # define the input tensor
+    imagenet_mean = 117.0
+    t_preprocessed = tf.expand_dims(t_input-imagenet_mean, 0)
+    tf.import_graph_def(sess.graph_def, {'input':t_preprocessed})
+    graph = sess.graph
+    Sess = sess	
+    # Process input image
+    img0 = PIL.Image.open('pilatus_flowers.jpg')
+    img0 = np.float32(img0)
+    layers = graph.get_operations()
+    tesnor = []
+    #for a in layers:
+       #tensor.append(a.name)
+    #for b in tensor:
+    render_deepdream(tf.square(T('inception_v3/mixed_8x8x2048b/concat')), img0)
+
+
+
+def evaluate(dataset):
+  """Evaluate model on Dataset for a number of steps."""
+  with tf.Graph().as_default():
+    # Get images and labels from the dataset.
+    images, labels = image_processing.inputs(dataset)
+
+    # Number of classes in the Dataset label set plus 1.
+    # Label 0 is reserved for an (unused) background class.
+    num_classes = dataset.num_classes() + 1
+
+    # Build a Graph that computes the logits predictions from the
+    # inference model.
+    logits, _ = inception.inference(images, num_classes)
+
+    # Calculate predictions.
+    top_1_op = tf.nn.in_top_k(logits, labels, 1)
+    top_5_op = tf.nn.in_top_k(logits, labels, 5)
+
+    # Restore the moving average version of the learned variables for eval.
+    variable_averages = tf.train.ExponentialMovingAverage(
+        inception.MOVING_AVERAGE_DECAY)
+    variables_to_restore = variable_averages.variables_to_restore()
+    saver = tf.train.Saver(variables_to_restore)
+
+    # Build the summary operation based on the TF collection of Summaries.
+    summary_op = tf.merge_all_summaries()
+
+    graph_def = tf.get_default_graph().as_graph_def()
+    summary_writer = tf.train.SummaryWriter(FLAGS.eval_dir,
+                                            graph_def=graph_def)
+
+    while True:
+      _eval_once(saver, summary_writer, top_1_op, top_5_op, summary_op)
+      if FLAGS.run_once:
+        break
+      time.sleep(FLAGS.eval_interval_secs)
+
